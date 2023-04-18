9acf2fe4b030c007dfb8f2663cce1061a8913f44
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2023-04-18 10:49:29 +0300

    Handling Exception logging better, including traceback

--- src/python3_commons/json.py
+++ src/python3_commons/json.py
@@ -8,7 +8,7 @@ from typing import Any
 class CustomJSONEncoder(json.JSONEncoder):
     def default(self, o) -> Any:
         try:
-            return json.JSONEncoder.default(self, o)
+            return super(CustomJSONEncoder, self).default(o)
         except TypeError:
             if isinstance(o, datetime):
                 return o.isoformat()
@@ -16,9 +16,7 @@ class CustomJSONEncoder(json.JSONEncoder):
                 return o.isoformat()
             elif isinstance(o, bytes):
                 return base64.b64encode(o).decode('ascii')
-            elif isinstance(o, socket):
-                return str(o)
-            elif isinstance(o, type):
+            elif isinstance(o, (socket, type, Exception)):
                 return str(o)
 
         return type(o).__name__
