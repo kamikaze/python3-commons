4dd31de78ab4b4e98cb6fcaa91077ab280ce6604
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2024-03-26 15:40:58 +0200

    Improved logging

--- src/python3_commons/logging/formatter.py
+++ src/python3_commons/logging/formatter.py
@@ -1,8 +1,12 @@
 import json
 import logging
 import traceback
+from contextvars import ContextVar
 
 from python3_commons.serializers.json import CustomJSONEncoder
+
+
+correlation_id: ContextVar[str | None] = ContextVar('correlation_id', default=None)
 
 
 class JSONFormatter(logging.Formatter):
@@ -11,6 +15,9 @@ class JSONFormatter(logging.Formatter):
         return ''.join(traceback.format_exception(*exc_info))
 
     def format(self, record):
+        if corr_id := correlation_id.get():
+            record.correlation_id = corr_id
+
         if record.exc_info:
             record.exc_text = self.format_exception(record.exc_info)
         else:
