7a708e78636c5af4f056e0ffa645cedba040172f
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-03-11 11:25:14 +0200

    Added user permission check for RBAC

--- src/python3_commons/permissions.py
+++ src/python3_commons/permissions.py
@@ -5,7 +5,7 @@ import sqlalchemy as sa
 from sqlalchemy import func, exists, and_
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from python3_commons.db.models import RBACPermission, RBACApiKeyRole, RBACRolePermission
+from python3_commons.db.models import RBACPermission, RBACApiKeyRole, RBACRolePermission, RBACUserRole
 
 logger = logging.getLogger(__name__)
 
@@ -17,6 +17,25 @@ async def has_api_key_permission(session: AsyncSession, api_key_uid: UUID, permi
                 RBACApiKeyRole.api_key_uid == api_key_uid,
                 (RBACApiKeyRole.expires_at.is_(None) | (RBACApiKeyRole.expires_at > func.now())),
                 RBACApiKeyRole.role_uid == RBACRolePermission.role_uid,
+                RBACRolePermission.permission_uid == RBACPermission.uid,
+                RBACPermission.name == permission
+            )
+        )
+    )
+
+    cursor = await session.execute(query)
+    result = cursor.scalar()
+
+    return result
+
+
+async def has_user_permission(session: AsyncSession, user_id: UUID, permission: str) -> bool:
+    query = sa.select(
+        exists().where(
+            and_(
+                RBACUserRole.user_id == user_id,
+                (RBACUserRole.expires_at.is_(None) | (RBACUserRole.expires_at > func.now())),
+                RBACUserRole.role_uid == RBACRolePermission.role_uid,
                 RBACRolePermission.permission_uid == RBACPermission.uid,
                 RBACPermission.name == permission
             )
