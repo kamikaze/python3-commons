939b0a7279449c9f40fee96e8af19c3b27922134
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-10-12 21:10:10 +0300

    Moving FastAPI related code to fastapi-commons

--- src/python3_commons/auth.py
+++ src/python3_commons/auth.py
@@ -1,13 +1,10 @@
 import logging
-from collections.abc import Callable, Coroutine, MutableMapping, Sequence
+from collections.abc import Sequence
 from http import HTTPStatus
-from typing import Annotated, Any, TypeVar
+from typing import TypeVar
 
 import aiohttp
 import msgspec
-from fastapi import Depends, HTTPException
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
-from jose import JWTError, jwt
 
 from python3_commons.conf import oidc_settings
 
@@ -23,7 +20,6 @@ class TokenData(msgspec.Struct):
 
 T = TypeVar('T', bound=TokenData)
 OIDC_CONFIG_URL = f'{oidc_settings.authority_url}/.well-known/openid-configuration'
-bearer_security = HTTPBearer(auto_error=oidc_settings.enabled)
 
 
 async def fetch_openid_config() -> dict:
@@ -32,9 +28,9 @@ async def fetch_openid_config() -> dict:
     """
     async with aiohttp.ClientSession() as session, session.get(OIDC_CONFIG_URL) as response:
         if response.status != HTTPStatus.OK:
-            raise HTTPException(
-                status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail='Failed to fetch OpenID configuration'
-            )
+            msg = 'Failed to fetch OpenID configuration'
+
+            raise RuntimeError(msg)
 
         return await response.json()
 
@@ -45,44 +41,8 @@ async def fetch_jwks(jwks_uri: str) -> dict:
     """
     async with aiohttp.ClientSession() as session, session.get(jwks_uri) as response:
         if response.status != HTTPStatus.OK:
-            raise HTTPException(status_code=HTTPStatus.INTERNAL_SERVER_ERROR, detail='Failed to fetch JWKS')
+            msg = 'Failed to fetch JWKS'
+
+            raise RuntimeError(msg)
 
         return await response.json()
-
-
-def get_token_verifier[T](
-    token_cls: type[T],
-    jwks: MutableMapping,
-) -> Callable[[HTTPAuthorizationCredentials], Coroutine[Any, Any, T | None]]:
-    async def get_verified_token(
-        authorization: Annotated[HTTPAuthorizationCredentials, Depends(bearer_security)],
-    ) -> T | None:
-        """
-        Verify the JWT access token using OIDC authority JWKS.
-        """
-        if not oidc_settings.enabled:
-            return None
-
-        token = authorization.credentials
-
-        try:
-            if not jwks:
-                openid_config = await fetch_openid_config()
-                _jwks = await fetch_jwks(openid_config['jwks_uri'])
-                jwks.clear()
-                jwks.update(_jwks)
-
-            if oidc_settings.client_id:
-                payload = jwt.decode(token, jwks, algorithms=['RS256'], audience=oidc_settings.client_id)
-            else:
-                payload = jwt.decode(token, jwks, algorithms=['RS256'])
-
-            token_data = token_cls(**payload)
-        except jwt.ExpiredSignatureError as e:
-            raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail='Token has expired') from e
-        except JWTError as e:
-            raise HTTPException(status_code=HTTPStatus.UNAUTHORIZED, detail=f'Token is invalid: {e!s}') from e
-
-        return token_data
-
-    return get_verified_token
--- src/python3_commons/db/models/rbac.py
+++ src/python3_commons/db/models/rbac.py
@@ -1,8 +1,6 @@
 import uuid
-from datetime import datetime
 
-from fastapi_users_db_sqlalchemy import GUID
-from sqlalchemy import CheckConstraint, DateTime, ForeignKey, PrimaryKeyConstraint, String
+from sqlalchemy import CheckConstraint, ForeignKey, PrimaryKeyConstraint, String
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import Mapped, mapped_column
 
@@ -40,52 +38,3 @@ class RBACRolePermission(Base):
     )
 
     __table_args__ = (PrimaryKeyConstraint('role_uid', 'permission_uid', name='pk_rbac_role_permissions'),)
-
-
-class RBACUserRole(Base):
-    __tablename__ = 'rbac_user_roles'
-
-    user_id: Mapped[uuid.UUID | None] = mapped_column(
-        GUID,
-        ForeignKey('users.id', name='fk_rbac_user_roles_user', ondelete='CASCADE'),
-        index=True,
-    )
-    role_uid: Mapped[uuid.UUID | None] = mapped_column(
-        UUID,
-        ForeignKey('rbac_roles.uid', name='fk_rbac_user_roles_role', ondelete='CASCADE'),
-        index=True,
-    )
-    starts_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
-    expires_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
-
-    __table_args__ = (PrimaryKeyConstraint('user_id', 'role_uid', name='pk_rbac_user_roles'),)
-
-
-class RBACApiKeyRole(Base):
-    __tablename__ = 'rbac_api_key_roles'
-
-    api_key_uid: Mapped[uuid.UUID | None] = mapped_column(
-        UUID,
-        ForeignKey('api_keys.uid', name='fk_rbac_api_key_roles_user', ondelete='CASCADE'),
-        index=True,
-    )
-    role_uid: Mapped[uuid.UUID | None] = mapped_column(
-        UUID,
-        ForeignKey('rbac_roles.uid', name='fk_rbac_api_key_roles_role', ondelete='CASCADE'),
-        index=True,
-    )
-    starts_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False)
-    expires_at: Mapped[datetime | None] = mapped_column(DateTime(timezone=True))
-
-    __table_args__ = (PrimaryKeyConstraint('api_key_uid', 'role_uid', name='pk_rbac_api_key_roles'),)
-
-
-# class RBACRoleRelation(Base):
-#     __tablename__ = 'rbac_role_relations'
-#
-#     parent_uid: Mapped[uuid.UUID] = mapped_column(UUID)
-#     child_uid: Mapped[uuid.UUID] = mapped_column(UUID)
-#
-#     __table_args__ = (
-#         PrimaryKeyConstraint('parent_uid', 'child_uid', name='pk_rbac_role_relations'),
-#     )
