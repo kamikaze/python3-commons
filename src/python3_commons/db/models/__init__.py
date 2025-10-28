e3220f338402ee73e2c3513b5650954e47cdbb28
Oleg Korsak <kamikaze.is.waiting.you@gmail.com>
2025-10-29 01:17:13 +0200

    removed rbac

--- src/python3_commons/db/models/__init__.py
+++ src/python3_commons/db/models/__init__.py
@@ -1,4 +1,1 @@
 from python3_commons.db.models.auth import UserGroup as UserGroup
-from python3_commons.db.models.rbac import RBACPermission as RBACPermission
-from python3_commons.db.models.rbac import RBACRole as RBACRole
-from python3_commons.db.models.rbac import RBACRolePermission as RBACRolePermission
