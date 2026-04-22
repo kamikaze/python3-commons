dbc05db1a894ba32389acde4f28b2ec85517840c
Olegs Korsaks <kamikaze.is.waiting.you@gmail.com>
2026-04-23 01:47:39 +0300

    Fixed object_storage.get_client()

--- src/python3_commons/object_storage.py
+++ src/python3_commons/object_storage.py
@@ -31,12 +31,9 @@ def get_client() -> ObjectStorageClient:
     if not _CLIENT:
         with _CLIENT_LOCK:
             if not _CLIENT:
-                client = ObjectStorageClient()
-                _CLIENT = client
-            else:
-                client = _CLIENT
+                _CLIENT: ObjectStorageClient = ObjectStorageClient()
 
-    return client
+    return _CLIENT
 
 
 class ObjectStorage(metaclass=SingletonMeta):
@@ -135,7 +132,7 @@ async def list_objects(bucket_name: str, prefix: str, *, recursive: bool = True)
 
 
 async def get_object_streams(
-    bucket_name: str, path: str, *, recursive: bool = True
+        bucket_name: str, path: str, *, recursive: bool = True
 ) -> AsyncGenerator[tuple[str, datetime, StreamingBody]]:
     async for obj in list_objects(bucket_name, path, recursive=recursive):
         object_name = obj['Key']
@@ -146,7 +143,7 @@ async def get_object_streams(
 
 
 async def get_objects(
-    bucket_name: str, path: str, *, recursive: bool = True
+        bucket_name: str, path: str, *, recursive: bool = True
 ) -> AsyncGenerator[tuple[str, datetime, bytes]]:
     async for object_name, last_modified, stream in get_object_streams(bucket_name, path, recursive=recursive):
         data = await stream.read()
@@ -168,7 +165,7 @@ async def remove_object(bucket_name: str, object_name: str) -> None:
 
 
 async def remove_objects(
-    bucket_name: str, prefix: str | None = None, object_names: Iterable[str] | None = None
+        bucket_name: str, prefix: str | None = None, object_names: Iterable[str] | None = None
 ) -> Sequence[Mapping] | None:
     storage = ObjectStorage(s3_settings)
 
@@ -191,7 +188,7 @@ async def remove_objects(
             chunk_size = 1000
 
             for i in range(0, len(objects_to_delete), chunk_size):
-                chunk = objects_to_delete[i : i + chunk_size]
+                chunk = objects_to_delete[i: i + chunk_size]
 
                 response = await s3_client.delete_objects(Bucket=bucket_name, Delete={'Objects': chunk})
 
